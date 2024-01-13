0016ac474b56c64e86b9fa98f6bf98dd1d031f34
Suliman-000 <sulimna.gt7@gmail.com>
2024-01-13 20:31:01 +0100

    Showing a Single Post on the Show page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Resources\PostResource;
+use Inertia\Inertia;
 
 class PostController extends Controller
 {
@@ -39,7 +40,11 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        $post->load('user');
+
+        return Inertia('Posts/Show', [
+            'post' => PostResource::make($post)
+        ]);
     }
 
     /**
--- tests/Feature/Controllers/PostController/IndexTest.php
+++ tests/Feature/Controllers/PostController/IndexTest.php
@@ -4,16 +4,10 @@ use App\Models\Post;
 use function Pest\Laravel\get;
 
 use App\Http\Resources\PostResource;
-use App\Http\Resources\UserResource;
-use Inertia\Testing\AssertableInertia;
-use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Http\Resources\Json\ResourceCollection;
 
 it('should return the correct component', function () {
     get(route('posts.index'))
-        ->assertInertia(fn (AssertableInertia $inertia) => $inertia
-            ->component('Posts/Index', true)
-    );
+        ->assertComponent('Posts/Index');
 });
 
 it('passes posts to the view', function () {
