03ffc72173ae6a1d9ffc9ccfbb769f6902590031
Suliman-000 <sulimna.gt7@gmail.com>
2024-05-07 02:19:29 +0200

    Created Post Polices, used shared data to wire the polices with the frond end.

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -41,6 +41,13 @@ const menu = [
         url: route('posts.index'),
         route: 'posts.index',
         // when: () => usePage().props.auth.user,
+    },
+
+    {
+        name: 'Create a Post',
+        url: route('posts.create'),
+        route: 'posts.create',
+        when: () => usePage().props.permissions.create_posts,
     }
 ];
 
