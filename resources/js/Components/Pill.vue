e5c1e58c94e53083189295945d8bfb959d3b1956
Suliman-000 <sulimna.gt7@gmail.com>
2024-05-17 23:20:18 +0200

    Upgraded to Laravel 11

--- resources/js/Components/Pill.vue
+++ resources/js/Components/Pill.vue
@@ -2,7 +2,7 @@
     <Link
         :href="href"
         class="whitespace-nowrap mb-2 rounded-full py-0.5 px-2 border border-pink-500 text-pink-500 hover:bg-indigo-500 hover:text-indigo-50"
-        :class="{'bg-indigo-500 text-indigo-50' : filled}"
+        :class="{'bg-indigo-500 !text-indigo-50' : filled}"
     >
             <slot></slot>
     </Link>
